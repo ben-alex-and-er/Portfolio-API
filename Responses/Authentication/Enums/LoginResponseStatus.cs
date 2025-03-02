31be928a3c269cd09c60d93e3a8d0e8deeee80c3
Ben Alexander <ben250402@yahoo.co.uk>
2025-03-02 17:22:33 +0000

    1 Added Response for Login

---
+++ Responses/Authentication/Enums/LoginResponseStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace Responses.Authentication.Enums
+{
+	/// <summary>
+	/// Status code for logging in a user
+	/// </summary>
+	public enum LoginResponseStatus
+	{
+		/// <summary>
+		/// Indicates the user logged in successfully
+		/// </summary>
+		SUCCESS = 0,
+
+		/// <summary>
+		/// Indicates the user could not be found
+		/// </summary>
+		USER_NOT_FOUND = 1,
+
+		/// <summary>
+		/// Indicates the user password was incorrect
+		/// </summary>
+		INCORRECT_PASSWORD = 2,
+
+		/// <summary>
+		/// Indicates the user failed to be logged in (for some unknown reason)
+		/// </summary>
+		FAILED = 3,
+	}
+}
---
+++ Responses/Authentication/LoginResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace Responses.Authentication
+{
+	using Enums;
+
+
+	/// <summary>
+	/// Response for logging in a user
+	/// </summary>
+	public class LoginResponse
+	{
+		/// <summary>
+		/// Represents the status result from loggin in
+		/// </summary>
+		public LoginResponseStatus Status { get; }
+
+		/// <summary>
+		/// JWT Token containing user claims
+		/// </summary>
+		public string? Token { get; }
+
+
+		/// <summary>
+		/// Constructor for <see cref="LoginResponse"/>
+		/// </summary>
+		/// <param name="status"></param>
+		/// <param name="token"></param>
+		public LoginResponse(LoginResponseStatus status, string? token = null)
+		{
+			Status = status;
+			Token = token;
+		}
+	}
+}
